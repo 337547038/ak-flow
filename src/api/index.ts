9049c09863ca2539fefdd4e890497b785bf600a5
337547038 <337547038@qq.com>
2025-10-17 14:44:49 +0800

    示例

--- src/api/index.ts
+++ src/api/index.ts
@@ -29,17 +29,28 @@ const allApi: any = {
     })
 }*/
 
-// 使用json演示数据
 export default (apiKey: string, data?: { [key: string]: any }) => {
-    const id: string = data?.id || ''
-    const includesApi = "flow/my,flow/todo,flowRecord/done,flowRecord/copy"
-    let userId: string = "";
-    if (includesApi.split(',').includes(allApi[apiKey] || apiKey)) {
-        userId = getUserInfo().id;
+    const host = window.location.host
+    // 使用json演示数据
+    if (host.indexOf('github.com') !== -1 || host.indexOf('localhost') !== -1) {
+        const id: string = data?.id || ''
+        const includesApi = "flow/my,flow/todo,flowRecord/done,flowRecord/copy"
+        let userId: string = "";
+        if (includesApi.split(',').includes(allApi[apiKey] || apiKey)) {
+            userId = getUserInfo().id;
+        }
+        return request({
+            url: `mock/${allApi[apiKey] || apiKey}${userId}${id}.json`,
+            method: 'get',
+            data
+        })
+    } else {
+        // 使用后端接口时
+        return request({
+            url: `api/${allApi[apiKey] || apiKey}`,
+            method: 'POST',
+            data
+        })
     }
-    return request({
-        url: `mock/${allApi[apiKey] || apiKey}${userId}${id}.json`,
-        method: 'get',
-        data
-    })
+
 }
